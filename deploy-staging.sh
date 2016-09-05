3dcbe0b63895163b785757e3a87ffdbbc55d109c
Louis Springer <louis.springer@viawest.com>
2016-09-05 16:12:07 -0600

    Support for staging and qa branches.

--- deploy-staging.sh
+++ deploy-staging.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-docker push louspringer/sample-node
+docker push louspringer/sample-node:staging
 
-ssh -p 10022 deploy@216.46.187.124 << EOF
-docker pull louspringer/sample-node:latest
+ssh -p 30022 deploy@216.46.187.124 << EOF
+docker pull louspringer/sample-node:staging
 docker stop web || true
 docker rm web || true
 docker rmi louspringer/sample-node:current || true
-docker tag louspringer/sample-node:latest louspringer/sample-node:current
+docker tag louspringer/sample-node:staging louspringer/sample-node:current
 docker run -d --net app --restart always --name web -p 3000:3000 louspringer/sample-node:current
 EOF
