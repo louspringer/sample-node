94c83c6dae565564aee36e609ba0b2bc32a100df
Louis Springer <louis.springer@viawest.com>
2016-09-07 10:17:43 -0600

    Fixed erroneous config.js switch statement.

--- config.js
+++ config.js
@@ -1,11 +1,9 @@
-switch(process.env.NODE_ENV) {
-    module.exports = {
-      client: "mysql",
-      connection: {
-        database: process.env.DB_NAME,
-        host:     process.env.DB_HOST,
-        user:     process.env.DB_USER,
-        password: process.env.DB_PASSWORD,
-      },
-    };
-}
+module.exports = {
+  client: "mysql",
+  connection: {
+    database: process.env.DB_NAME,
+    host:     process.env.DB_HOST,
+    user:     process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+};
